2c2974604bfe90b83ea440bf4c9f0c3035ccb7d4
gauravgudvar <121889010+gauravgudvar@users.noreply.github.com>
2025-01-10 18:03:43 +0530

    introduce bug

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,6 +1,6 @@
-package hello;
+//package hello;
 
-public class Greeter {
+//public class Greeter {
     public String sayHello() {
         return "DevOps Project for AchiStar Technologies";
     }
